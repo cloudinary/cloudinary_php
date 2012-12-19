2c1ed97c703f2ced010e9691a0b11a9f0826121f
Alon Hammerman <the.alonso@gmail.com>
2012-12-19 11:14:28 +0200

    Check signature of direct upload

--- samples/PhotoAlbum/upload_complete.php
+++ samples/PhotoAlbum/upload_complete.php
@@ -2,6 +2,21 @@
 require 'main.php';
 # Here should do whatever authentication / verification needed
 
-# Create model using the data received
-$photo = \PhotoAlbum\create_photo_model($_POST["public_id"], $_POST["format"]);
+# Check for authentic cloudinary response
+$api_secret = Cloudinary::config_get("api_secret");
+if (!$api_secret) throw new \InvalidArgumentException("Must supply api_secret");
+$existing_signature = \Cloudinary::option_consume($_POST, "signature");
+$to_sign = array(
+    'public_id' => $_POST['public_id'],
+    'version' => $_POST['version'],
+);
+$calculated_signature = \Cloudinary::api_sign_request($to_sign, $api_secret);
+
+if ($existing_signature == $calculated_signature) {
+    $photo = \PhotoAlbum\create_photo_model($_POST["public_id"], $_POST["format"]);
+} else {
+    error_log("Given data's signature can't be verified (" .
+        $existing_signature . " != " . $calculated_signature . "). data: " .
+        \PhotoAlbum\ret_var_dump(_POST));
+}
 ?>
