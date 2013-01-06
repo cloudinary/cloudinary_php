82afd56f3b6dbc5583fa00dbb917fd745870721b
Alon Hammerman <the.alonso@gmail.com>
2013-01-07 01:44:39 +0200

    add documentation, pretty up layout a bit, add facebook and fetch demos

--- samples/PhotoAlbum/list.php
+++ samples/PhotoAlbum/list.php
@@ -23,9 +23,43 @@
     </script>
   </head>
   <body>
+    <div id="logo">
+        <!-- This will render the fetched image using cloudinary -->
+        <?php echo fetch_image_tag("http://cloudinary.com/images/logo.png") ?>
+    </div>
+    <div id="posterframe" style="position: absolute; right: 0; top: 5px;">
+        <!-- This will render the fetched facebook image using cloudinary with all the
+             requested transformations -->
+        <?php echo facebook_profile_image_tag("officialchucknorrispage", array(
+          "format" => "png",
+          "transformation" => array(
+            array("height" => 95, "width" => 95, "crop" => "thumb", "gravity" => "face",
+              "effect" => "sepia", "radius" => 20
+            ), array("angle" => 10)
+          ))); 
+        ?>
+    </div>
+    <h1>Welcome!</h1>
+    <p>This is the main demo page of the PhotoAlbum sample PHP application for Cloudinary.<br />
+    Here you can see all the photos you had uploaded to the app and find some information about how
+    to implement your own PHP application storing, manipulating and serving your photos using 
+    Cloudinary!</p>
+
+    <p>All of the images you see here are served by cloudinary. For instance the logo and the posterframe. They are both render from server side using the Cloudinary shortcut functions: fetch_image_tag and facebook_profile_image_tag. These two pictures weren't even have to be uploaded to cloudinary, they are retrieved by the backend, transformed, cached and distributed using a CDN.
+    </p>
+
     <h1>Photos</h1>
     <div class="photos">
+      <?php if (R::count('photo') > 0) { ?>
+        <p>Following are the photos uploaded by you. If you want to upload more pictures go to the
+        <a href="upload.php">upload page</a>.<br />
+        You can click each picture to view its original size, and see more info about it by clicking
+        <i>show more...</i></p>
+      <?php } else { ?>
+        <p>No images were uploaded yet. click <a href="upload.php">here</a>
+         to go to the upload page</p>
       <?php
+        }
         foreach (R::findAll('photo') as $photo) {
       ?>
         <div class="photo">
@@ -40,7 +74,7 @@
           </div>
           <div class="more_info">
             <a href="#" class="toggle_info">Show less...</a>
-            <div class="thumbnails">
+            <table class="thumbnails">
               Different thumbnails:
               <?php 
                 $thumbs = array(
@@ -55,17 +89,18 @@
                   )),
                 );
                 foreach($thumbs as $params) {
-                  echo cl_image_tag($photo["public_id"], array_merge($thumbs_params, $params));
+                  $merged_params = array_merge($thumbs_params, $params);
+                  echo "<td>";
+                  echo cl_image_tag($photo["public_id"], $merged_params);
+                  echo "Params: ";
+                  print_r($merged_params);
+                  echo "</td>";
                 }
-                /*
-                echo cl_image_tag($photo["public_id"], array(
-                  array_merge($thumbs_params, array("crop" => "fill", "gravity" => "north")),
-                  array("angle" => "20"),
-                  array("effect" => "sepia"),
-                ));
-                */
               ?>
-            </div>
+            </table>
+            <p> As you can see, different tranformations can go a long way.
+            You're welcome to consult the <a href="http://cloudinary.com/documentation/image_transformations"
+            >Image Transformation reference</a> for more awesome transformations
             <?php 
               foreach ($photo as $key => $value) {
                 echo "<p>$key = $value</p>";
