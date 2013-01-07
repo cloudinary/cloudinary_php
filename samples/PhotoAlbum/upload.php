59175d8f04d3742fd56993b5e2f4334c77092238
Alon Hammerman <the.alonso@gmail.com>
2013-01-07 02:03:36 +0200

    pretty up upload.php

--- samples/PhotoAlbum/upload.php
+++ samples/PhotoAlbum/upload.php
@@ -3,51 +3,92 @@ require 'main.php';
 ?>
 <!DOCTYPE HTML>
 <html>
-<head>
-<meta charset="utf-8">
-<title>jQuery File Upload Example</title>
-</head>
-<body>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<script src="lib/jquery.ui.widget.js"></script>
-<script src="lib/jquery.iframe-transport.js"></script>
-<script src="lib/jquery.fileupload.js"></script>
-<script src="lib/jquery.cloudinary.js"></script>
+  <head>
+    <meta charset="utf-8">
+    <title>jQuery File Upload Example</title>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script src="lib/jquery.ui.widget.js"></script>
+    <script src="lib/jquery.iframe-transport.js"></script>
+    <script src="lib/jquery.fileupload.js"></script>
+    <script src="lib/jquery.cloudinary.js"></script>
+  </head>
+  <body>
 
-<div id='backend_upload'>
-    <br /><h1>Upload through backend</h1>
-    <input id="fileupload" type="file" name="files[]" data-url="upload_backend.php" multiple>
-</div>
+    <div id="logo">
+        <!-- This will render the fetched image using cloudinary -->
+        <?php echo fetch_image_tag("http://cloudinary.com/images/logo.png") ?>
+    </div>
+    <div id="posterframe" style="position: absolute; right: 0; top: 5px;">
+        <!-- This will render the fetched facebook image using cloudinary with all the
+             requested transformations -->
+        <?php echo facebook_profile_image_tag("officialchucknorrispage", array(
+          "format" => "png",
+          "transformation" => array(
+            array("height" => 95, "width" => 95, "crop" => "thumb", "gravity" => "face",
+              "effect" => "sepia", "radius" => 20
+            ), array("angle" => 10)
+          ))); 
+        ?>
+    </div>
 
-<script>
-$(function () {
-    $('#fileupload').fileupload({
-        dataType: 'json',
-        dropZone: '#backend_upload',
-        done: function (e, data) {
-            $.each(data.result.files, function (index, file) {
-                $('<p/>').text(file.name).appendTo('#backend_upload');
-            });
-        }
-    });
-});
-</script>
+    <!-- This is the backend upload - based on the fileupload javascript for the frontend and also
+         on the PHP backend demo. The uploaded images will be processed in upload_backend.php -->
+    <div id='backend_upload'>
+      <br /><h1>Upload through backend</h1>
+      <input id="fileupload" type="file" name="files[]" data-url="upload_backend.php" multiple>
+    </div>
 
-<div id='direct_upload'>
-    <br /><h1>Direct upload</h1>
-    <?php
-      echo cl_image_upload_tag('test', array("tags" => "direct_photo_album"));
-    ?>
-</div>
-<script>
-    $('.cloudinary-fileupload')
-    .fileupload({ dropZone: '#direct_upload' })
-    .on('cloudinarydone', function (e, data) {
-        console.log("cloudinarydone: ", data);
-        $.post('upload_complete.php', data.result);
-        $('<p/>').text(data.result.public_id).appendTo('#direct_upload');
-    });
-</script>
-</body> 
+    <script>
+      $(function () {
+          $('#fileupload').fileupload({
+              dataType: 'json',
+              dropZone: '#backend_upload',
+              start: function () {
+                $('.status_value').text('Starting backend upload...');
+              },
+              progress: function () {
+                $('.status_value').text('Continue backend upload...');
+              },
+              done: function (e, data) {
+                $('.status_value').text('Idle');
+                $.each(data.result.files, function (index, file) {
+                  $('<p/>').text(file.name).appendTo('#backend_upload');
+                });
+              }
+          });
+      });
+    </script>
+
+    <div class="status" style="position: fixed; width: 200px; left: 50%; text-align: center; border: double;">
+      <h3>Status</h3>
+      <span class="status_value">Idle</span>
+    </div>
+    <!-- This is the direct upload - also based on the fileupload for the frontend. 
+          The cl_image_upload_tag php function is generating the required html and javascript to
+          allow uploading directly to your Cloudinary storage -->
+    <div id='direct_upload'>
+      <br /><h1>Direct upload</h1>
+      <?php
+        echo cl_image_upload_tag('test', array("tags" => "direct_photo_album"));
+      ?>
+    </div>
+    <script>
+      $('.cloudinary-fileupload')
+      .fileupload({ 
+        dropZone: '#direct_upload',
+        start: function () {
+          $('.status_value').text('Starting direct upload...');
+        },
+        progress: function () {
+          $('.status_value').text('Continue direct upload...');
+        },
+      })
+      .on('cloudinarydone', function (e, data) {
+          $('.status_value').text('Idle');
+          $.post('upload_complete.php', data.result);
+          $('<p/>').text(data.result.public_id).appendTo('#direct_upload');
+      });
+    </script>
+  </body> 
 </html>
 
--- samples/basic/basic.php
+++ samples/basic/basic.php
@@ -1,8 +1,9 @@
 <?php
+include '../../src/Cloudinary.php';
+include '../../src/Uploader.php';
 if (file_exists('settings.php')) {
   include 'settings.php';
 }
-include 'src/Uploader.php';
 
 $sample_paths = array(
   "pizza" => getcwd(). "\\pizza.jpg",
@@ -10,33 +11,48 @@ $sample_paths = array(
   "couple" => "http://res.cloudinary.com/demo/image/upload/couple.jpg",
 );
 $default_thumbs = array(
-  "width" => 50,
-  "height" => 80,
+  "width" => 200,
+  "height" => 150,
 );
 $default_upload_options = array("tags" => "basic_sample");
-$files = array();
-$files["unnamed_local"] = \Cloudinary\Uploader::upload($sample_paths["pizza"],
-  $default_upload_options);
-$files["named_local"] = \Cloudinary\Uploader::upload($sample_paths["pizza"],
-  array_merge($default_upload_options, array("public_id" => "named")));
 $eager_params = array_merge($default_thumbs, array("crop" => "scale"));
-$files["eager"] = \Cloudinary\Uploader::upload($sample_paths["lake"],
-  array_merge($default_upload_options, array(
-    "public_id" => "eager",
-    "eager" => $eager_params,
-  )
-));
-$files["remote"] = \Cloudinary\Uploader::upload($sample_paths["couple"],
-  $default_upload_options);
-$files["remote_trans"] = \Cloudinary\Uploader::upload($sample_paths["couple"],
-  array_merge($default_upload_options, array(
-    "public_id" => "transformed",
-    "width" => 500,
-    "height" => 500,
-    "crop" => "fit",
-    "effect" => "saturation:-70",
-  ))
-);
+$files = array();
+
+# This function, when called uploads all files into your Cloudinary storage and saves the
+# metadata to the $files array.
+function do_uploads() {
+  global $files, $sample_paths, $default_thumbs, $default_upload_options, $eager_params;
+  # public_id will be generated on Cloudinary backend.
+  $files["unnamed_local"] = \Cloudinary\Uploader::upload($sample_paths["pizza"],
+    $default_upload_options);
+  # Same image, uploaded with a public_id
+  $files["named_local"] = \Cloudinary\Uploader::upload($sample_paths["pizza"],
+    array_merge($default_upload_options, array("public_id" => "named")));
+
+  # Eager tranformations are applied as soon as the file is uploaded, instead of waiting
+  # for a user to request them. 
+  $files["eager"] = \Cloudinary\Uploader::upload($sample_paths["lake"],
+    array_merge($default_upload_options, array(
+      "public_id" => "eager",
+      "eager" => $eager_params,
+    )
+  ));
+  # In the two following examples, the file is fetched from a remote URL and stored in Cloudinary.
+  # This allows you to apply the same transformations, and serve those using Cloudinary's CDN.
+  $files["remote"] = \Cloudinary\Uploader::upload($sample_paths["couple"],
+    $default_upload_options);
+  $files["remote_trans"] = \Cloudinary\Uploader::upload($sample_paths["couple"],
+    array_merge($default_upload_options, array(
+      "public_id" => "transformed",
+      "width" => 500,
+      "height" => 500,
+      "crop" => "fit",
+      "effect" => "saturation:-70",
+    ))
+  );
+}
+
+# Output an image in HTML along with provided caption and public_id
 function show_image($img, $options = array(), $caption = "") {
     echo "<td><div class='caption'>" . $caption . "</div>";
     $options["format"] = $img["format"];
@@ -48,19 +64,24 @@ function show_image($img, $options = array(), $caption = "") {
 ?>
 <html>
   <body>
+    <?php
+      echo "<h2>Uploading ... </h2>";
+      do_uploads();
+      echo "<h3>... Done!</h3>";
+    ?>
     <table style="text-align: center;">
       <tr>
         <?php
           show_image($files["unnamed_local"], array_merge($default_thumbs,
-            array("crop" => "fill")), "unnamed");
+            array("crop" => "fill")), "local unnamed, crop - fill");
           show_image($files["named_local"], array_merge($default_thumbs, 
-            array("crop" => "fit")), "named");
-          show_image($files["eager"], $eager_params, "eager");
+            array("crop" => "fit")), "local named, crop - fit");
+          show_image($files["eager"], $eager_params, "local with eager tranformations");
           show_image($files["remote"], array_merge($default_thumbs, 
-            array("crop" => "thumb", "gravity" => "face")), "remote face");
+            array("crop" => "thumb", "gravity" => "faces")), "fetch remotely + crop gravity faces");
           show_image($files["remote_trans"], array_merge($default_thumbs, 
             array("crop" => "fill", "gravity" => "face", "radius" => 10)),
-            "remote with effect");
+            "fetch remotely with effects");
         ?>
       </tr>
     </table>
