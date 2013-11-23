ca200e3a6f934db224b1f6153eee1e2964e21002
Alon Hammerman <the.alonso@gmail.com>
2013-11-22 20:55:30 -0600

    php framework - add extended_identifier to PreloadedFile

--- src/Uploader.php
+++ src/Uploader.php
@@ -283,7 +283,10 @@ namespace Cloudinary {
 			return "v" . $this->version . "/" . $this->filename;
 		}
 		
-		  
+		public function extended_identifier() {
+			return $this->resource_type . "/" . $this->type . "/" . $this->identifier();
+		}
+
 	    public function __toString() {
 			return $this->resource_type . "/" . $this->type . "/v" . $this->version . "/" . $this->filename . "#" . $this->signature;
 		}
