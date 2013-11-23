dae0ba1b8a5c1684e08dfd035c73f3254f771683
Alon Hammerman <the.alonso@gmail.com>
2013-11-23 15:51:34 -0600

    php framework - Use DIRECTORY_SEPARATOR

--- src/Uploader.php
+++ src/Uploader.php
@@ -183,7 +183,7 @@ namespace Cloudinary {
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_TIMEOUT, 60);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $post_params);
-            curl_setopt($ch, CURLOPT_CAINFO,realpath(dirname(__FILE__))."/cacert.pem");
+            curl_setopt($ch, CURLOPT_CAINFO,realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR."cacert.pem");
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $response = curl_exec($ch);
             $curl_error = NULL;
