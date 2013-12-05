5f9ee0a562f344558159031599e75091faa04068
Rasmus Werling <rasmus.werling@druid.fi>
2013-12-05 09:09:54 +0200

    Added trim() to filter away parameters with only empty space(s).

--- src/Cloudinary.php
+++ src/Cloudinary.php
@@ -141,7 +141,7 @@ class Cloudinary {
             $params[$param] = Cloudinary::option_consume($options, $option);
         }
 
-        $param_filter = function($value) { return $value === 0 || $value === '0' || $value == true; };
+        $param_filter = function($value) { return $value === 0 || $value === '0' || trim($value) == true; };
         $params = array_filter($params, $param_filter);
         ksort($params);
         $join_pair = function($key, $value) { return $key . "_" . $value; };
