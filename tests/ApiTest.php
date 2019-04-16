61ca569bb0a41c9a9bc235cf42a8d6bc8d1cd1f8
Brian John Luk <brian@cloudinary.com>
2019-04-16 12:57:15 -0700

    added unit tests

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -382,13 +382,40 @@ namespace Cloudinary {
          *
          * @throws Api\GeneralError
          */
-        public function test_resource_quality_analysis()
+        public function test07a_resource_quality_analysis()
         {
             $resource = $this->api->resource(self::$api_test, ["quality_analysis" => true]);
             $this->assertArrayHasKey("quality_analysis", $resource);
             $this->assertInternalType("double", $resource["quality_analysis"]["focus"]);
         }
 
+        /**
+         * Should allow next_cursor parameter
+         *
+         * @throws Api\GeneralError
+         */
+        public function test07b_resource_allows_next_cursor_parameter()
+        {
+            Curl::mockApi($this);
+
+            $this->api->resource(self::$api_test, ["next_cursor" => "foo"]);
+            assertGet($this);
+            assertParam($this, "next_cursor", "foo");
+        }
+
+        /**
+         * Should allow derived_next_cursor parameter
+         *
+         * @throws Api\GeneralError
+         */
+        public function test07c_resource_allows_derived_next_cursor_parameter()
+        {
+            Curl::mockApi($this);
+
+            $this->api->resource(self::$api_test, ["derived_next_cursor" => "foo"]);
+            assertGet($this);
+            assertParam($this, "derived_next_cursor", "foo");
+        }
         /**
          * Should allow deleting derived resource
          *
