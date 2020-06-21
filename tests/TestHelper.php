b4d617053695265391decb9ce7885a4551219478
Constantine Nathanson <constantine@cloudinary.com>
2020-06-21 17:43:01 +0300

    Add support of retries in unit tests

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -303,4 +303,67 @@ END;
             empty($message) ? 'Should correctly encode JSON into the HTTP request' : $message
         );
     }
+
+    /**
+     * Trait RetryTrait
+     * @package Cloudinary
+     */
+    trait RetryTrait
+    {
+        public function runBare()
+        {
+            $e = null;
+
+            $numberOfRetires = $this->getNumberOfRetries();
+            if (false === is_numeric($numberOfRetires)) {
+                throw new \LogicException(sprintf(
+                    'The $numberOfRetires must be a number but got "%s"',
+                    var_export($numberOfRetires, true)
+                ));
+            }
+            $numberOfRetires = (int)$numberOfRetires;
+            if ($numberOfRetires <= 0) {
+                throw new \LogicException(sprintf(
+                    'The $numberOfRetires must be a positive number greater than 0 but got "%s".',
+                    $numberOfRetires
+                ));
+            }
+
+            for ($i = 0; $i < $numberOfRetires; ++$i) {
+                try {
+                    parent::runBare();
+
+                    return;
+                } catch (\PHPUnit_Framework_IncompleteTestError $e) {
+                    throw $e;
+                } catch (\PHPUnit_Framework_SkippedTestError $e) {
+                    throw $e;
+                } catch (\Exception $e) {
+                    // last one thrown below
+                }
+            }
+
+            if ($e) {
+                throw $e;
+            }
+        }
+
+        /**
+         * @return int
+         */
+        private function getNumberOfRetries()
+        {
+            $annotations = $this->getAnnotations();
+
+            if (isset($annotations['method']['retry'][0])) {
+                return $annotations['method']['retry'][0];
+            }
+
+            if (isset($annotations['class']['retry'][0])) {
+                return $annotations['class']['retry'][0];
+            }
+
+            return 1;
+        }
+    }
 }
