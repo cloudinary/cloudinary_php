6401f880d989c2ea30902c09a3beaf4a3ff3abe1
RTLcoil <oleg@rtl.co.il>
2020-10-07 13:46:11 +0300

    Add test helper to determine whether to test an addon

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -369,4 +369,55 @@ END;
             return 1;
         }
     }
+
+    class AddOn
+    {
+        const ALL                         = 'all';                       // Test all addons.
+        const ASPOSE                      = 'aspose';                    // Aspose document conversion.
+        const AZURE                       = 'azure';                     // Microsoft azure video indexer.
+        const BG_REMOVAL                  = 'bgremoval';                 // Cloudinary AI background removal.
+        const FACIAL_ATTRIBUTES_DETECTION = 'facialattributesdetection'; // Advanced facial attributes detection.
+        const GOOGLE                      = 'google';                    // Google AI video moderation, google AI
+                                                                         // video transcription, google auto tagging,
+                                                                         // google automatic video tagging,
+                                                                         // google translation.
+        const IMAGGA                      = 'imagga';                    // Imagga auto tagging, crop and scale.
+        const JPEGMINI                    = 'jpegmini';                  // JPEGmini image optimization.
+        const LIGHTROOM                   = 'lightroom';                 // Adobe photoshop lightroom (BETA).
+        const METADEFENDER                = 'metadefender';              // MetaDefender anti-malware protection.
+        const NEURAL_ARTWORK              = 'neuralartwork';             // Neural artwork style transfer.
+        const OBJECT_AWARE_CROPPING       = 'objectawarecropping';       // Cloudinary object-aware cropping.
+        const OCR                         = 'ocr';                       // OCR text detection and extraction.
+        const PIXELZ                      = 'pixelz';                    // Remove the background.
+        const REKOGNITION                 = 'rekognition';               // Amazon rekognition AI moderation,
+                                                                         // amazon rekognition auto tagging,
+                                                                         // amazon rekognition celebrity detection.
+        const URL2PNG                     = 'url2png';                   // URL2PNG website screenshots.
+        const VIESUS                      = 'viesus';                    // VIESUS automatic image enhancement.
+        const WEBPURIFY                   = 'webpurify';                 // WebPurify image moderation.
+
+        /**
+         * Should a certain add on be tested.
+         *
+         * @param string $add_on
+         *
+         * @return bool
+         */
+        public static function should_test_add_on($add_on)
+        {
+            $cld_test_add_ons = strtolower(getenv('CLD_TEST_ADDONS'));
+            if ($cld_test_add_ons === self::ALL) {
+                return true;
+            }
+
+            return in_array(
+                strtolower($add_on),
+                array_map(
+                    'trim',
+                    explode(',', $cld_test_add_ons)
+                ),
+                false
+            );
+        }
+    }
 }
