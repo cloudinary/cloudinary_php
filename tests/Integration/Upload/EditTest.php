37c0514f72804bb963fde05077d2158aed808dda
RTLcoil <oleg@rtl.co.il>
2021-02-28 22:18:27 +0200

    Expand testing of `rename` in the Upload API

--- tests/Integration/Upload/EditTest.php
+++ tests/Integration/Upload/EditTest.php
@@ -11,8 +11,11 @@
 namespace Cloudinary\Test\Integration\Upload;
 
 use Cloudinary\Api\Exception\ApiError;
+use Cloudinary\Api\Exception\BadRequest;
 use Cloudinary\Api\Exception\NotFound;
 use Cloudinary\Asset\AssetTransformation;
+use Cloudinary\Test\Helpers\MockUploadApi;
+use Cloudinary\Test\Helpers\RequestAssertionsTrait;
 use Cloudinary\Test\Integration\IntegrationTestCase;
 use Cloudinary\Test\Unit\Asset\AssetTestCase;
 use Cloudinary\Transformation\Gravity;
@@ -23,10 +26,17 @@ use Cloudinary\Transformation\Resize;
  */
 final class EditTest extends IntegrationTestCase
 {
+    use RequestAssertionsTrait;
+
     const TRANSFORMATION_STRING = 'c_crop,g_face,h_400,w_400';
 
-    private static $SOURCE_PUBLIC_ID;
-    private static $RENAME_PUBLIC_ID;
+    private static $RENAME_SOURCE_PUBLIC_ID;
+    private static $RENAME_TARGET_PUBLIC_ID;
+    private static $RENAME_TO_EXISTING_SOURCE_PUBLIC_ID;
+    private static $RENAME_TO_EXISTING_TARGET_PUBLIC_ID;
+    private static $RENAME_TO_EXISTING_SOURCE_OVERWRITE_PUBLIC_ID;
+    private static $RENAME_TO_EXISTING_TARGET_OVERWRITE_PUBLIC_ID;
+    private static $CHANGE_TYPE_PUBLIC_ID;
     private static $DESTROY_PUBLIC_ID;
     private static $EXPLICIT_PUBLIC_ID;
     private static $TRANSFORMATION_OBJECT;
@@ -38,15 +48,24 @@ final class EditTest extends IntegrationTestCase
     {
         parent::setUpBeforeClass();
 
-        self::$SOURCE_PUBLIC_ID = 'upload_edit_source_public_id_' . self::$UNIQUE_TEST_ID;
-        self::$RENAME_PUBLIC_ID = 'upload_edit_rename_public_id_' . self::$UNIQUE_TEST_ID;
+        self::$RENAME_SOURCE_PUBLIC_ID             = 'upload_edit_rename_source_public_id_' . self::$UNIQUE_TEST_ID;
+        self::$RENAME_TARGET_PUBLIC_ID             = 'upload_edit_rename_target__public_id_' . self::$UNIQUE_TEST_ID;
+        self::$RENAME_TO_EXISTING_SOURCE_PUBLIC_ID = 'upload_edit_rename_to_existing_source_public_id_' . self::$UNIQUE_TEST_ID;
+        self::$RENAME_TO_EXISTING_TARGET_PUBLIC_ID = 'upload_edit_rename_to_existing_target_public_id_' . self::$UNIQUE_TEST_ID;
+        self::$RENAME_TO_EXISTING_SOURCE_OVERWRITE_PUBLIC_ID = 'upload_edit_rename_to_existing_overwrite_source_public_id_' . self::$UNIQUE_TEST_ID;
+        self::$RENAME_TO_EXISTING_TARGET_OVERWRITE_PUBLIC_ID = 'upload_edit_rename_to_existing_overwrite_target_public_id_' . self::$UNIQUE_TEST_ID;
+        self::$CHANGE_TYPE_PUBLIC_ID = 'upload_edit_change_type_public_id_' . self::$UNIQUE_TEST_ID;
         self::$DESTROY_PUBLIC_ID = 'upload_edit_destroy_public_id_' . self::$UNIQUE_TEST_ID;
         self::$EXPLICIT_PUBLIC_ID = 'upload_edit_explicit_public_id_' . self::$UNIQUE_TEST_ID;
         self::$TRANSFORMATION_OBJECT = (new AssetTransformation())->resize(Resize::crop(400, 400, Gravity::face()));
 
         self::uploadTestAssetImage(['public_id' => self::$EXPLICIT_PUBLIC_ID]);
         self::uploadTestAssetImage(['public_id' => self::$DESTROY_PUBLIC_ID]);
-        self::uploadTestAssetImage(['public_id' => self::$SOURCE_PUBLIC_ID]);
+        self::uploadTestAssetImage(['public_id' => self::$RENAME_SOURCE_PUBLIC_ID]);
+        self::uploadTestAssetImage(['public_id' => self::$RENAME_TO_EXISTING_SOURCE_PUBLIC_ID]);
+        self::uploadTestAssetImage(['public_id' => self::$RENAME_TO_EXISTING_TARGET_PUBLIC_ID]);
+        self::uploadTestAssetImage(['public_id' => self::$RENAME_TO_EXISTING_SOURCE_OVERWRITE_PUBLIC_ID]);
+        self::uploadTestAssetImage(['public_id' => self::$RENAME_TO_EXISTING_TARGET_OVERWRITE_PUBLIC_ID]);
     }
 
     public static function tearDownAfterClass()
@@ -74,13 +93,68 @@ final class EditTest extends IntegrationTestCase
      */
     public function testRenameImage()
     {
-        $asset = self::$uploadApi->rename(self::$SOURCE_PUBLIC_ID, self::$RENAME_PUBLIC_ID);
+        $asset = self::$uploadApi->rename(self::$RENAME_SOURCE_PUBLIC_ID, self::$RENAME_TARGET_PUBLIC_ID);
 
-        self::assertValidAsset($asset, ['public_id' => self::$RENAME_PUBLIC_ID]);
+        self::assertValidAsset($asset, ['public_id' => self::$RENAME_TARGET_PUBLIC_ID]);
 
-        $asset = self::$adminApi->asset(self::$RENAME_PUBLIC_ID);
+        $asset = self::$adminApi->asset(self::$RENAME_TARGET_PUBLIC_ID);
 
-        self::assertValidAsset($asset, ['public_id' => self::$RENAME_PUBLIC_ID]);
+        self::assertValidAsset($asset, ['public_id' => self::$RENAME_TARGET_PUBLIC_ID]);
+    }
+
+    /**
+     * Try to rename an image to an existing image public id
+     */
+    public function testRenameToExistingImagePublicId()
+    {
+        $this->expectException(BadRequest::class);
+        self::$uploadApi->rename(
+            self::$RENAME_TO_EXISTING_SOURCE_PUBLIC_ID,
+            self::$RENAME_TO_EXISTING_TARGET_PUBLIC_ID
+        );
+    }
+
+    /**
+     * Rename an image to an existing image public id, overwriting existing images
+     */
+    public function testRenameAndOverwriteToExistingImagePublicId()
+    {
+        $resource = self::$uploadApi->rename(
+            self::$RENAME_TO_EXISTING_SOURCE_OVERWRITE_PUBLIC_ID,
+            self::$RENAME_TO_EXISTING_TARGET_OVERWRITE_PUBLIC_ID,
+            ['overwrite' => true]
+        );
+
+        self::assertValidAsset($resource, ['public_id' => self::$RENAME_TO_EXISTING_TARGET_OVERWRITE_PUBLIC_ID]);
+
+        $resource = self::$adminApi->asset(self::$RENAME_TO_EXISTING_TARGET_OVERWRITE_PUBLIC_ID);
+
+        self::assertValidAsset($resource, ['public_id' => self::$RENAME_TO_EXISTING_TARGET_OVERWRITE_PUBLIC_ID]);
+    }
+
+    /**
+     * Change an image type
+     */
+    public function testChangeImageType()
+    {
+        $mockUploadApi = new MockUploadApi();
+        $mockUploadApi->rename(
+            self::$CHANGE_TYPE_PUBLIC_ID,
+            self::$CHANGE_TYPE_PUBLIC_ID,
+            ['to_type' => 'private']
+        );
+        $lastRequest = $mockUploadApi->getMockHandler()->getLastRequest();
+
+        self::assertRequestUrl($lastRequest, '/image/rename');
+        self::assertRequestPost($lastRequest);
+        self::assertRequestBodySubset(
+            $lastRequest,
+            [
+                'to_type' => 'private',
+                'from_public_id' => self::$CHANGE_TYPE_PUBLIC_ID,
+                'to_public_id' => self::$CHANGE_TYPE_PUBLIC_ID
+            ]
+        );
     }
 
     /**
