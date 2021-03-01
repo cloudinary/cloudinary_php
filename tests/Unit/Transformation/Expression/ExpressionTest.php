7ef9bd4e45ff2b3e1063787bdb43ba414e61779c
RTLcoil <RTLcoil@users.noreply.github.com>
2021-03-01 04:23:12 +0200

    Fix expression normalisation

--- tests/Unit/Transformation/Expression/ExpressionTest.php
+++ tests/Unit/Transformation/Expression/ExpressionTest.php
@@ -13,6 +13,7 @@ namespace Cloudinary\Test\Unit\Transformation\Expression;
 use Cloudinary\Transformation\Crop;
 use Cloudinary\Transformation\Expression\Expression;
 use Cloudinary\Transformation\Expression\ExpressionComponent;
+use Cloudinary\Transformation\Expression\ExpressionUtils;
 use Cloudinary\Transformation\Expression\LogicalOperator;
 use Cloudinary\Transformation\Expression\PVar;
 use Cloudinary\Transformation\Expression\RelationalOperator;
@@ -116,5 +117,63 @@ final class ExpressionTest extends TestCase
         self::assertEquals($expectedExpr, (string)$rawExpr);
 
         self::assertEquals($expectedExpr . '_gt_997', (string)$rawExpr->greaterThan()->numeric(997));
+    }
+
+    /**
+     * Check expression normalization
+     *
+     * @dataProvider testNormalizationDataProvider
+     *
+     * @param mixed       $input          Value to normalize
+     * @param null|string $expectedOutput Expected normalized output
+     */
+    public function testExpressionNormalization($input, $expectedOutput)
+    {
+        $actual = ExpressionUtils::normalize($input);
+        self::assertEquals($expectedOutput, $actual);
+    }
+
+    /**
+     * Data provider for testExpressionNormalization
+     *
+     * @return array[]
+     */
+    public static function testNormalizationDataProvider()
+    {
+        return [
+            'null is not affected' => [null, null],
+            'number replaced with a string value' => [10, '10'],
+            'empty string is not affected' => ['', ''],
+            'single space is replaced with a single underscore' => [' ', '_'],
+            'blank string is replaced with a single underscore' => ['   ', '_'],
+            'underscore is not affected' => ['_', '_'],
+            'sequence of underscores and spaces is replaced with a single underscore' => [' _ __  _', '_'],
+            'arbitrary text is not affected' => ['foobar', 'foobar'],
+            'double ampersand replaced with and operator' => ['foo && bar', 'foo_and_bar'],
+            'double ampersand with no space at the end is not affected' => ['foo&&bar', 'foo&&bar'],
+            'width recognized as variable and replaced with w' => ['width', 'w'],
+            'initial aspect ratio recognized as variable and replaced with iar' => ['initial_aspect_ratio', 'iar'],
+            '$width recognized as user variable and not affected' => ['$width', '$width'],
+            '$initial_aspect_ratio recognized as user variable followed by aspect_ratio variable' => [
+                '$initial_aspect_ratio',
+                '$initial_ar',
+            ],
+            '$mywidth recognized as user variable and not affected' => ['$mywidth', '$mywidth'],
+            '$widthwidth recognized as user variable and not affected' => ['$widthwidth', '$widthwidth'],
+            '$_width recognized as user variable and not affected' => ['$_width', '$_width'],
+            '$__width recognized as user variable and not affected' => ['$__width', '$_width'],
+            '$$width recognized as user variable and not affected' => ['$$width', '$$width'],
+            '$height recognized as user variable and not affected' => ['$height_100', '$height_100'],
+            '$heightt_100 recognized as user variable and not affected' => ['$heightt_100', '$heightt_100'],
+            '$$height_100 recognized as user variable and not affected' => ['$$height_100', '$$height_100'],
+            '$heightmy_100 recognized as user variable and not affected' => ['$heightmy_100', '$heightmy_100'],
+            '$myheight_100 recognized as user variable and not affected' => ['$myheight_100', '$myheight_100'],
+            '$heightheight_100 recognized as user variable and not affected' => [
+                '$heightheight_100',
+                '$heightheight_100',
+            ],
+            '$theheight_100 recognized as user variable and not affected' => ['$theheight_100', '$theheight_100'],
+            '$__height_100 recognized as user variable and not affected' => ['$__height_100', '$_height_100']
+        ];
     }
 }
